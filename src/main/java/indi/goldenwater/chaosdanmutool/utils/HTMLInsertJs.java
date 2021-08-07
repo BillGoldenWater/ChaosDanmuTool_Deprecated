182ff1cb176cb11b85a558014a0a4c10faf3e923
BiliGoldenWater <2439577268@qq.com>
2021-08-07 20:54:39 +0800

    add HTMLInsertJs

---
+++ src/main/java/indi/goldenwater/chaosdanmutool/utils/HTMLInsertJs.java
@@ -0,0 +1,11 @@
+package indi.goldenwater.chaosdanmutool.utils;
+
+public class HTMLInsertJs {
+    public static String getJsDanmuList(String html) {
+        return getJs("danmuList", html);
+    }
+
+    public static String getJs(String listId, String html) {
+        return String.format("document.getElementById(\"%s\").insertAdjacentHTML(\"%s\");", listId, html);
+    }
+}
