b0f0b8908c1316abe9f3e0bcecac6e8b26060cd1
BiliGoldenWater <2439577268@qq.com>
2021-08-14 22:05:34 +0800

    add SUPER_CHAT_MESSAGE

---
+++ src/main/java/indi/goldenwater/chaosdanmutool/model/danmu/SuperChatMessage.java
@@ -0,0 +1,32 @@
+package indi.goldenwater.chaosdanmutool.model.danmu;
+
+public class SuperChatMessage extends MessageCommand {
+    public int roomid;
+    public String background_bottom_color;
+    public String background_color;
+    public String background_color_end;
+    public String background_color_start;
+    public String background_icon;
+    public String background_image;
+    public String background_price_color;
+    public double color_point;
+    public int dmscore;
+    public long end_time;
+    public Object gift;
+    public long id;
+    public int is_ranked;
+    public String is_send_audit;
+    public MedalInfo medal_info;
+    public String message;
+    public String message_font_color;
+    public String message_trans;
+    public int price;
+    public int rate;
+    public long start_time;
+    public int time;
+    public String token;
+    public int trans_mark;
+    public long ts;
+    public long uid;
+    public UserInfo user_info;
+}
---
+++ src/main/java/indi/goldenwater/chaosdanmutool/model/danmu/UserInfo.java
@@ -0,0 +1,16 @@
+package indi.goldenwater.chaosdanmutool.model.danmu;
+
+public class UserInfo {
+    public String face;
+    public String face_frame;
+    public int guard_level;
+    public int is_main_vip;
+    public int is_svip;
+    public int is_vip;
+    public String level_color;
+    public int manager;
+    public String name_color;
+    public String title;
+    public String uname;
+    public int user_level;
+}
