accea6df1db61ef3eea0f583a8d0ae917e0ef305
BiliGoldenWater <2439577268@qq.com>
2021-08-29 00:42:17 +0800

    remove icon in guard buy message

--- src/main/java/indi/goldenwater/chaosdanmutool/model/html/GuardBuyHTML.java
+++ src/main/java/indi/goldenwater/chaosdanmutool/model/html/GuardBuyHTML.java
@@ -8,7 +8,6 @@ import indi.goldenwater.chaosdanmutool.model.danmu.GuardBuy;
 public class GuardBuyHTML extends DanmuItemHTML {
     private static final String guardBuyTemplate = "" +
             "<span style=\"color: {{textColor}}\"> 购买了<span style=\"color: {{giftNameColor}};\"> {{gift_name}} </span>" +
-            "<img class=\"gift-icon\" src=\"{{gift_icon_url}}\" alt=\"\">" +
             " x {{gift_num}} {{price}}" +
             "</span>";
 
@@ -26,7 +25,6 @@ public class GuardBuyHTML extends DanmuItemHTML {
                 .replace("{{textColor}}", config.internalViewConfig.style.danmuContent.textColor)
                 .replace("{{giftNameColor}}", "#ffff00")
                 .replace("{{gift_name}}", guardBuy.gift_name != null ? guardBuy.gift_name : "未知")
-                .replace("{{gift_icon_url}}", giftData.img_basic)
                 .replace("{{gift_num}}", String.valueOf(guardBuy.num))
                 .replace("{{price}}", "￥" + (guardBuy.price / 1000));
         return start.replace("class=\"danmu-item\"", "class=\"danmu-item danmu-gift\"")
