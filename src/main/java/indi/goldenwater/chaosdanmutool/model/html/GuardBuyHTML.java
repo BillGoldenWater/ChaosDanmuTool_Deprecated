8d605bd3246b330b34addc4e8821f1911d6df07d
BiliGoldenWater <2439577268@qq.com>
2021-09-09 17:33:08 +0800

    fix GuardBuy null exception change version to 0.2.6

--- src/main/java/indi/goldenwater/chaosdanmutool/model/html/GuardBuyHTML.java
+++ src/main/java/indi/goldenwater/chaosdanmutool/model/html/GuardBuyHTML.java
@@ -2,19 +2,16 @@ package indi.goldenwater.chaosdanmutool.model.html;
 
 import indi.goldenwater.chaosdanmutool.ChaosDanmuTool;
 import indi.goldenwater.chaosdanmutool.config.Config;
-import indi.goldenwater.chaosdanmutool.model.GiftDataList;
 import indi.goldenwater.chaosdanmutool.model.danmu.GuardBuy;
 
 public class GuardBuyHTML extends DanmuItemHTML {
     private static final String guardBuyTemplate = "" +
             "<span style=\"color: {{textColor}}\"> 购买了<span style=\"color: {{giftNameColor}};\"> {{gift_name}} </span>" +
-            "<img class=\"gift-icon\" src=\"{{gift_icon_url}}\" alt=\"\">" +
             " x {{gift_num}} {{price}}" +
             "</span>";
 
     public static String parse(GuardBuy guardBuy) {
         final Config config = ChaosDanmuTool.getConfig();
-        final GiftDataList.GiftData giftData = GiftDataList.giftList.get(guardBuy.gift_id);
 
         String userInfoHTML = UserInfoHTML.parse(null,
                 false,
@@ -26,7 +23,6 @@ public class GuardBuyHTML extends DanmuItemHTML {
                 .replace("{{textColor}}", config.internalViewConfig.style.danmuContent.textColor)
                 .replace("{{giftNameColor}}", "#ffff00")
                 .replace("{{gift_name}}", guardBuy.gift_name != null ? guardBuy.gift_name : "未知")
-                .replace("{{gift_icon_url}}", giftData.img_basic)
                 .replace("{{gift_num}}", String.valueOf(guardBuy.num))
                 .replace("{{price}}", "￥" + (guardBuy.price / 1000));
         return start.replace("class=\"danmu-item\"", "class=\"danmu-item danmu-gift\"")
